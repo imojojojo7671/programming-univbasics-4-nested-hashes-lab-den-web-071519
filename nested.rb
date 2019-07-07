e71e6105fa7544feaaaf44ff8dea54a45a92b485
Drew Peterson <drew.a.peterson@hotmail.com>
2019-07-07 23:28:30 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -66,10 +66,7 @@ return programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
-programmer_hash[:yukihiro_matsumoto] = {
-	   :known_for => "Ruby",
-	   :languages => ["LISP", "C"]
-	   }
+
 
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
@@ -93,6 +90,10 @@ programmer_hash[:yukihiro_matsumoto] = {
         }
      }
 
+		 programmer_hash[:yukihiro_matsumoto] = {
+		 	   :known_for => "Ruby",
+		 	   :languages => ["LISP", "C"]
+		 	   }
 
 end
 
